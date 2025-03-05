278914c6043d354f8d0c569091d6b1d8276e0a8c
Henrique Mosseri <henriquemosseri@gmail.com>
2025-03-05 12:29:40 -0300

    Segundo commit- Interação com usuário

--- app/src/main/java/henriquemosseri/com/github/helloworld/MainActivity.kt
+++ app/src/main/java/henriquemosseri/com/github/helloworld/MainActivity.kt
@@ -2,6 +2,9 @@ package henriquemosseri.com.github.helloworld
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +19,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val etName = findViewById<EditText>(R.id.etName)
+        val tvHelloMessage = findViewById<TextView>(R.id.tvHelloMessage)
 
+        // Listener para quando o usuário pressiona Enter (actionDone)
+        etName.setOnEditorActionListener { textView, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val name = textView.text.toString().trim()
+                // Verifica se o nome foi digitado
+                if (name.isNotEmpty()) {
+                    tvHelloMessage.text = "Hello World, $name!"
+                } else {
+                    tvHelloMessage.text = "Hello World!"
+                }
+                true // Indica que consumimos o evento
+            } else {
+                false
+            }
+        }
     }
 }
