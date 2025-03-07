c9c4d6d16032a3b9bdb75933513d103b91622729
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-06 21:37:11 -0300

    corrigindo sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "henriquemosseri.com.github.helloworld"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "henriquemosseri.com.github.helloworld"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
